c232f49219f52cebe71f976abd544ea6189d5d98
IOelinton <oelinton.dearaujo@gmail.com>
2025-02-12 05:35:18 -0300

    fix: refazendo a estilização

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,63 @@
-import React from "react";
-
+import { useState, useEffect } from "react";
+import Loading from "./Loading.jsx";
 import "../styles/body.css";
 
-export default function Body() {
+const Body = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isReady, setIsReady] = useState(false);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      setIsReady(false);
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=1025"
+      );
+      const data = await response.json();
+      const pokemonDetails = await Promise.all(
+        data.results.map(async (pokemon) => {
+          const res = await fetch(pokemon.url);
+          return res.json();
+        })
+      );
+      setPokemons(pokemonDetails);
+      setLoading(false);
+
+      setTimeout(() => {
+        setIsReady(true);
+      }, 500);
+    } catch (error) {
+      console.error("Error fetching pokemons:", error);
+      setLoading(false);
+      setIsReady(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  if (loading || !isReady) {
+    return <Loading />;
+  }
+
   return (
-    <div className="body-container">
-      <h1>Body</h1>
+    <div>
+      <div className="pokemon-grid">
+        {pokemons.map((pokemon) => (
+          <div key={pokemon.id} className="pokemon-card">
+            <h3 className="pokemon-id"> {pokemon.id} </h3>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <h3 className="pokemon-name">{pokemon.name}</h3>
+            <h2 className="pokemon-type">
+              {pokemon.types.map((typeInfo) => typeInfo.type.name).join(" / ")}
+            </h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
+
+export default Body;
